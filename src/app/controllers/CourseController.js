6e618541922581fa78cdb8216ddf4f70b4e6ed9a
Viet Anh <vietanha9a2@gmail.com>
2022-12-02 22:34:05 +0700

    Update

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,6 +10,42 @@ class CourseController {
       )
       .catch(next);
   }
+
+  create(req, res) {
+    res.render("courses/create");
+  }
+
+  //[POST] /course/store
+  store(req, res) {
+    const formData = req.body;
+    formData.image = `https://i.ytimg.com/vi/${formData.videoId}/hqdefault.jpg`;
+    const course = new Course(formData);
+    course
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((err) => {});
+
+    res.send("Course saved!");
+    // res.render("courses/store");
+  }
+  //[GET] /courses/:id/edit
+  edit(req, res, next) {
+    Course.findById(req.params.id)
+      .then((course) =>
+        res.render("courses/edit", { course: mongooseToObject(course) })
+      )
+      .catch(next);
+  }
+
+  //[PUT] /courses/:id
+  update(req, res, next) {
+    const data = req.body;
+    data.image = `https://i.ytimg.com/vi/${data.videoId}/hqdefault.jpg`;
+    const course = new Course(data);
+    Course.updateOne({ _id: req.params.id }, data)
+      .then(() => res.redirect("/me/stored/courses"))
+      .catch(next);
+  }
 }
 
 module.exports = new CourseController();
